4055d1f4bbf2cde9e67b94ba75457783bd7ff870
Olivier <habimanaolivier6@gmail.com>
2020-02-24 14:41:27 +0200

    Fix Index CORS policy

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://gambino-backend.herokuapp.com"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, authorization"
