6e41073b791d39af693b06423e83116c364c1d20
Olivier <habimanaolivier6@gmail.com>
2020-02-24 13:37:50 +0200

    Fix Database config

--- src/config/config.js
+++ src/config/config.js
@@ -23,10 +23,10 @@ module.exports = {
   },
 
   production: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false }
+    }
   }
 };
